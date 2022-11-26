a7c30c6e873fe06446cf55066b2fae6fa51f6c9d
guantai <jpguantai@gmail.com>
2022-11-26 17:34:25 +0300

    Completed assignment

---
+++ src/dist/index.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+//A delete function that will remove tasks from your list
+// A priority value selected from a dropdown Links to an external site.that is used to determine the color of the text in the list (e.g. red for high priority, yellow for medium, green for low)
+// As an additional challenge, implement a sorting functionality that displays the tasks in ascending or descending order based on priority
+// An additional input field (e.g. user, duration, date due)
+//Ability to edit tasks
+//The main objective is to add a feature that allows the user's input to affect the DOM
+document.addEventListener("DOMContentLoaded", function () {// your code here
+  //A delete function that will remove tasks from your list
+});
--- src/index.js
+++ src/index.js
@@ -1,3 +1,139 @@
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskList = new TaskList();
+ 
+
+  const newTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority");
+
+  const taskUl = document.getElementById("tasks");
+
+  const renderApp = () => (taskUl.innerHTML = taskList.renderTasks());
+
+
+  newTaskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    taskList.createNewTask(newTaskDescription.value);
+  
+    e.target.reset();
+    renderApp();
+  });
+
+  taskUl.addEventListener("click", (e) => {
+    if (e.target.nodeName === "BUTTON") {
+      taskList.deleteTask(e.target.dataset.description);
+      renderApp();
+    }
+  });
 });
+
+class Task {
+  constructor(description) {
+    this.description = description;
+  }
+
+  render() {
+    return `
+      <li>
+        ${this.description}
+        <button data-description="${this.description}">X</button>
+      </li>
+      `;
+  }
+}
+
+class TaskList {
+  constructor() {
+    this.tasks = [];
+  }
+
+  createNewTask(description) {
+    const newTask = new Task(description);
+    this.tasks.push(newTask);
+  }
+
+  renderTasks() {
+    return this.tasks.map((task) => task.render()).join("");
+  }
+
+  deleteTask(description) {
+    this.tasks = this.tasks.filter((task) => task.description !== description);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   // const form = document.getElementById("create-task-form");
+  // const taskList = document.getElementById("tasks");
+  // const taskDescription = document.getElementById("new-task-description");
+  // const taskPriority = document.getElementById("new-task-priority");
+  // const taskPriorityValue = taskPriority.options[taskPriority.selectedIndex] .value;
+  // const taskPriorityColor = taskPriority.options[taskPriority.selectedIndex] .text;
+  // const taskPriorityColorValue = taskPriority.options[taskPriority.selectedIndex] .value;
+  // const taskPriorityColorText = taskPriority.options[taskPriority.selectedIndex] .text;
+  
+
+  // form.addEventListener("submit", function(event) {
+
+
+  //   event.preventDefault();
+
+
+  //   const newTask = document.createElement("li");
+  //   newTask.innerText = taskDescription.value;
+  //   taskList.appendChild(newTask);
+  //   event.target.reset();
+
+  //   const deleteButton = document.createElement("button");
+  //   deleteButton.innerText = "X";
+  //   newTask.appendChild(deleteButton);
+
+  //   deleteButton.addEventListener("click", function(event) {
+  //     newTask.remove();
+
+  //   });
+
+
+  //   }
+  // );
